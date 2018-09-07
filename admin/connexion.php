c8938b462df8482a5c0418dc4965ba4214095f6d
Damien Santo <39194178+DamienThiago93230@users.noreply.github.com>
2018-09-07 16:54:54 +0200

    Comit 04
    fds

--- admin/connexion.php
+++ admin/connexion.php
@@ -8,7 +8,7 @@ $user='root'; // le nom de l'utilisateur pour se connecter
 $password='';   // le mot de passe de l'utilisateur local (sur PC)
 // $password='root' // mot de passe local (sur MAC)
 
-$pdoCV = new PDO('mysql:host' . $host . ';dbname=' . $database,$user,$password);
+$pdoCV = new PDO('mysql:host=' . $host . ';dbname=' . $database,$user,$password);
 // $pdoCV est le nom de la variable pour la connexion à la BDD qui nous sert partout où l'on doit se servir de cette connexion
 $pdoCV->exec("SET NAMES utf8");
 
