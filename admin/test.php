b58fc8133f576a3023eb39478bcfeca856cc3a43
Damien Santo <39194178+DamienThiago93230@users.noreply.github.com>
2018-10-08 09:25:01 +0200

    C05
    Préparation base de données siteCV

--- admin/test.php
+++ admin/test.php
@@ -7,17 +7,41 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <?php 
-    $sql = $pdoCV->query(" SELECT * FROM t_users ");
-    $line_user = $sql->fetch();
-
-    
+        // Requête pour une seul info
+        $sql = $pdoCV->query(" SELECT * FROM t_utilisateurs "); 
+        $ligne_utilisateur = $sql->fetch();    
     ?>
-    <title>Admin : <?php echo $line_user['pseudo']; ?></title>
+    <title>Admin : <?php echo $ligne_utilisateur['pseudo']; ?></title>
     <link rel="stylesheet" href="test_style.css">
 </head>
 <body>
     <h1>Test en cours</h1>
-    <?php echo $line_user['first_name']; ?>
+    <p> <?php echo $ligne_utilisateur['nom'].' '. $ligne_utilisateur['prenom']; ?> </p> 
+    <hr>   
+    <?php
+        // Requête pour compter et chercher plusieurs enregistrements on ne peut compter qui si on a préparer(avec : prepare) la rrequête
+        $sql = $pdoCV -> prepare("SELECT * FROM t_loisirs");
+        $sql -> execute();
+        $nbr_loisirs = $sql -> rowCount();
+    ?>
+    <h5>Il y a <?php echo $nbr_loisirs; ?> loisirs</h5>
+    <ul>
+        <?php
+            while ($ligne_loisir = $sql -> fetch()) 
+            {
+        ?>
+            <li><?php echo $ligne_loisir['loisir']; ?></li>
+        <?php 
+            }// Fin du while  
+        ?>
+    </ul>
+
+
+
+
+
+     
+    
     
 </body>
 </html>
