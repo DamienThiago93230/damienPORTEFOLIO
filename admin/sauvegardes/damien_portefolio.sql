8d0ca09517defa309295d084003a4cdea5160c74
Damien Santo <39194178+DamienThiago93230@users.noreply.github.com>
2018-10-18 09:08:18 +0200

    C14
    create database

--- admin/sauvegardes/damien_portefolio.sql
+++ admin/sauvegardes/damien_portefolio.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1
--- Généré le :  ven. 07 sep. 2018 à 14:34
+-- Généré le :  jeu. 18 oct. 2018 à 09:07
 -- Version du serveur :  10.1.31-MariaDB
 -- Version de PHP :  7.2.4
 
@@ -23,6 +23,152 @@ SET time_zone = "+00:00";
 --
 CREATE DATABASE IF NOT EXISTS `damien_portefolio` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
 USE `damien_portefolio`;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `t_competences`
+--
+
+CREATE TABLE `t_competences` (
+  `id_competence` int(3) NOT NULL,
+  `competence` varchar(150) NOT NULL,
+  `niveau` int(3) NOT NULL,
+  `categorie` enum('Développement','Gestion de projet','Infographie') NOT NULL,
+  `id_utilisateur` int(3) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Déchargement des données de la table `t_competences`
+--
+
+INSERT INTO `t_competences` (`id_competence`, `competence`, `niveau`, `categorie`, `id_utilisateur`) VALUES
+(9, 'CSS', 90, 'Développement', 1),
+(18, 'HTML', 82, 'Développement', 1),
+(19, 'JavaScript', 70, 'Développement', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `t_experiences`
+--
+
+CREATE TABLE `t_experiences` (
+  `id_experience` int(3) NOT NULL,
+  `titre_exp` varchar(150) NOT NULL,
+  `stitre_exp` varchar(200) NOT NULL,
+  `dates_exp` varchar(100) NOT NULL,
+  `description_exp` text NOT NULL,
+  `id_utilisateur` int(3) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Déchargement des données de la table `t_experiences`
+--
+
+INSERT INTO `t_experiences` (`id_experience`, `titre_exp`, `stitre_exp`, `dates_exp`, `description_exp`, `id_utilisateur`) VALUES
+(7, 'p', 'p', 'p', 'p', 1),
+(8, 'p', 'p', 'iiiiiii', '22', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `t_formations`
+--
+
+CREATE TABLE `t_formations` (
+  `id_formation` int(3) NOT NULL,
+  `titre_form` varchar(150) NOT NULL,
+  `stitre_form` varchar(250) NOT NULL,
+  `dates_form` varchar(100) NOT NULL,
+  `description_form` text NOT NULL,
+  `id_utilisateur` int(3) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Déchargement des données de la table `t_formations`
+--
+
+INSERT INTO `t_formations` (`id_formation`, `titre_form`, `stitre_form`, `dates_form`, `description_form`, `id_utilisateur`) VALUES
+(16, 'CAP', 'Peintre en batiment', '2005-2006', 'IFIDEC à Pantin', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `t_loisirs`
+--
+
+CREATE TABLE `t_loisirs` (
+  `id_loisir` int(3) NOT NULL,
+  `loisir` varchar(250) NOT NULL,
+  `id_utilisateur` int(3) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Déchargement des données de la table `t_loisirs`
+--
+
+INSERT INTO `t_loisirs` (`id_loisir`, `loisir`, `id_utilisateur`) VALUES
+(1, 'Football', 1),
+(2, 'Pêche a la ligne', 1),
+(3, 'MMA', 1),
+(4, 'Boxe', 1),
+(5, 'Moto Cross', 1),
+(21, 'h', 2);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `t_messages`
+--
+
+CREATE TABLE `t_messages` (
+  `id_message` int(3) NOT NULL,
+  `nom` varchar(200) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `sujet` varchar(150) NOT NULL,
+  `message` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `t_realisations`
+--
+
+CREATE TABLE `t_realisations` (
+  `id_realisation` int(3) NOT NULL,
+  `titre_real` varchar(150) NOT NULL,
+  `stitre_real` varchar(200) NOT NULL,
+  `dates_real` varchar(100) NOT NULL,
+  `description_real` text NOT NULL,
+  `id_utilisateur` int(3) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `t_reseaux`
+--
+
+CREATE TABLE `t_reseaux` (
+  `id_reseau` int(3) NOT NULL,
+  `url` varchar(120) NOT NULL,
+  `id_utilisateur` int(3) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `t_titres`
+--
+
+CREATE TABLE `t_titres` (
+  `id_titre` int(3) NOT NULL,
+  `titre` varchar(250) NOT NULL,
+  `accroche` tinyint(255) NOT NULL,
+  `id_utilisateur` int(3) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -47,7 +193,7 @@ CREATE TABLE `t_users` (
   `zip` varchar(5) NOT NULL,
   `city` varchar(30) NOT NULL,
   `country` varchar(30) NOT NULL,
-  `comments` int(11) NOT NULL
+  `comments` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
@@ -55,11 +201,94 @@ CREATE TABLE `t_users` (
 --
 
 INSERT INTO `t_users` (`id_user`, `first_name`, `last_name`, `email`, `phone_1`, `phone_2`, `password`, `pseudo`, `age`, `birthday`, `gender`, `civilstatus`, `address`, `zip`, `city`, `country`, `comments`) VALUES
-(1, 'Thiago', 'Kaylie', 'thiagoKaylie@hotmail.com', '0606060606', '0601010101', 'azerty', 'azerty', 30, '2017-12-04', 'Homme', 'M', '2 rue des étoiles ', '93230', 'Romainville', 'France', 0);
+(1, 'Thiago', 'Kaylie', 'thiagoKaylie@hotmail.com', '0606060606', '0601010101', 'azerty', 'Thiago', 30, '2017-12-04', 'Homme', 'M', '2 rue des étoiles ', '93230', 'Romainville', 'France', '0');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `t_utilisateurs`
+--
+
+CREATE TABLE `t_utilisateurs` (
+  `id_utilisateur` int(3) NOT NULL,
+  `prenom` varchar(45) NOT NULL,
+  `nom` varchar(45) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `telephone` varchar(20) NOT NULL,
+  `portable` varchar(20) NOT NULL,
+  `mdp` varchar(15) NOT NULL,
+  `pseudo` varchar(15) NOT NULL,
+  `age` smallint(5) NOT NULL,
+  `anniversaire` date NOT NULL,
+  `genre` enum('Homme','Femme') NOT NULL,
+  `civilite` enum('M','Mme') NOT NULL,
+  `adresse` text NOT NULL,
+  `code_postal` varchar(5) NOT NULL,
+  `ville` varchar(30) NOT NULL,
+  `pays` varchar(30) NOT NULL,
+  `commentaire` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Déchargement des données de la table `t_utilisateurs`
+--
+
+INSERT INTO `t_utilisateurs` (`id_utilisateur`, `prenom`, `nom`, `email`, `telephone`, `portable`, `mdp`, `pseudo`, `age`, `anniversaire`, `genre`, `civilite`, `adresse`, `code_postal`, `ville`, `pays`, `commentaire`) VALUES
+(1, 'Damien', 'Santo', 'damien.santo@hotmail.com', '0618122564', '0601010101', 'azerty', 'Thiago', 30, '1987-12-04', 'Homme', 'M', 'Place de la laïcité', '93230', 'Romainville', 'France', '0'),
+(2, 'tintin', 'milou', 'milou@live.fr', '010101010101', '01010101010', 'azerty2', 'tintin', 30, '2018-10-02', 'Homme', 'M', 'uytrertyuklmùluiytrertyuioo', '93230', 'pantin', 'france', 'yujikjhgrthyjukki'),
+(3, 'tintin', 'milou', 'milou@live.fr', '010101010101', '01010101010', 'azerty2', 'tintin', 30, '2018-10-02', 'Homme', 'M', 'uytrertyuklmùluiytrertyuioo', '93230', 'pantin', 'france', 'yujikjhgrthyjukki');
 
 --
 -- Index pour les tables déchargées
 --
+
+--
+-- Index pour la table `t_competences`
+--
+ALTER TABLE `t_competences`
+  ADD PRIMARY KEY (`id_competence`);
+
+--
+-- Index pour la table `t_experiences`
+--
+ALTER TABLE `t_experiences`
+  ADD PRIMARY KEY (`id_experience`);
+
+--
+-- Index pour la table `t_formations`
+--
+ALTER TABLE `t_formations`
+  ADD PRIMARY KEY (`id_formation`);
+
+--
+-- Index pour la table `t_loisirs`
+--
+ALTER TABLE `t_loisirs`
+  ADD PRIMARY KEY (`id_loisir`);
+
+--
+-- Index pour la table `t_messages`
+--
+ALTER TABLE `t_messages`
+  ADD PRIMARY KEY (`id_message`);
+
+--
+-- Index pour la table `t_realisations`
+--
+ALTER TABLE `t_realisations`
+  ADD PRIMARY KEY (`id_realisation`);
+
+--
+-- Index pour la table `t_reseaux`
+--
+ALTER TABLE `t_reseaux`
+  ADD PRIMARY KEY (`id_reseau`);
+
+--
+-- Index pour la table `t_titres`
+--
+ALTER TABLE `t_titres`
+  ADD PRIMARY KEY (`id_titre`);
 
 --
 -- Index pour la table `t_users`
@@ -68,14 +297,74 @@ ALTER TABLE `t_users`
   ADD PRIMARY KEY (`id_user`);
 
 --
+-- Index pour la table `t_utilisateurs`
+--
+ALTER TABLE `t_utilisateurs`
+  ADD PRIMARY KEY (`id_utilisateur`);
+
+--
 -- AUTO_INCREMENT pour les tables déchargées
 --
+
+--
+-- AUTO_INCREMENT pour la table `t_competences`
+--
+ALTER TABLE `t_competences`
+  MODIFY `id_competence` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
+
+--
+-- AUTO_INCREMENT pour la table `t_experiences`
+--
+ALTER TABLE `t_experiences`
+  MODIFY `id_experience` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT pour la table `t_formations`
+--
+ALTER TABLE `t_formations`
+  MODIFY `id_formation` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+
+--
+-- AUTO_INCREMENT pour la table `t_loisirs`
+--
+ALTER TABLE `t_loisirs`
+  MODIFY `id_loisir` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
+
+--
+-- AUTO_INCREMENT pour la table `t_messages`
+--
+ALTER TABLE `t_messages`
+  MODIFY `id_message` int(3) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `t_realisations`
+--
+ALTER TABLE `t_realisations`
+  MODIFY `id_realisation` int(3) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `t_reseaux`
+--
+ALTER TABLE `t_reseaux`
+  MODIFY `id_reseau` int(3) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `t_titres`
+--
+ALTER TABLE `t_titres`
+  MODIFY `id_titre` int(3) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT pour la table `t_users`
 --
 ALTER TABLE `t_users`
   MODIFY `id_user` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT pour la table `t_utilisateurs`
+--
+ALTER TABLE `t_utilisateurs`
+  MODIFY `id_utilisateur` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
