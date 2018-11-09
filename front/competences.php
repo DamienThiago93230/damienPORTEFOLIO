bd526ddf35b5c7af32d90f925169f597b52c8d49
Damien Santo <39194178+DamienThiago93230@users.noreply.github.com>
2018-11-09 16:44:21 +0100

    C30
    travail page index

--- front/competences.php
+++ front/competences.php
@@ -45,7 +45,7 @@ require 'connexion.php';
     <div class="jumbotron text-center mb-4">
         <h1 class="display-4">Vous êtes sur la page <br> compétences</h1>
         <hr class="my-4">
-        <p >Découvrez les compétences que j'ai acquis au court de ma formation .</p>
+        <p style="color: black">Découvrez les compétences que j'ai acquis au court de ma formation .</p>
         <!-- <p class="icon">
             <i class="fab fa-php"></i><i class="fab fa-html5"></i><i class="fab fa-css3-alt"></i><i class="fab fa-js-square"></i><i class="fab fa-wordpress-simple"></i>
 
